e2b7d124ee46720c3af6bdec6df71a0723e78cc6
Ivan Ushakov <Ivan.MoLoT@gmail.com>
2012-11-24 10:30:30 +0400

    Initial Commit

---
+++ photo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  photo
+//
+//  Created by Иван Ушаков on 24.11.12.
+//  Copyright (c) 2012 Иван Ушаков. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
